f20d3daa3fc57c9f64da65e9a430da3708bc8623
min23asdw <min23asdw23asdw@outlook.com>
2020-12-17 16:27:16 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 int main(){
+  int x,y=1;
+  int Even=0,Odd=0;
+  while(y!=0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    if(x!=0){
+    if(x%2==0){Even=Even+1;}else{Odd=Odd+1;}
+  }
+    else{y=0;}
+   }
+    cout << "#Even numbers = "<< Even << "\n";
+    cout << "#Odd numbers = "<<Odd;
     return 0;
 }
