89f113a4a93ead67248e343179e4b29823a8d67f
min23asdw <min23asdw23asdw@outlook.com>
2020-12-18 00:19:42 +0700

    :D

--- test.cpp
+++ test.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int cheetah(int bath){
   int bot = bath/10; //จำนวนขวด
   int count=bot; //นับจำนวนฝาขวด
-  int x =bot;
   int i=0; //ลูป
+if(bot>=3){
 while(i<count || count >= 3){
   if(i%3==0){
     bot=bot+1;
@@ -12,16 +12,16 @@ while(i<count || count >= 3){
   }
   i++;
 }
-std::cout << count<<"\n";
 return bot;
+}else return bot;
 }
 
 
 
 
-int main(){
 
-cout << cheetah(204);
+int main(){
+cout << cheetah(35);
 
 
   return 0;
