f06384bf363afedf014ee3432a47b6e532f424a2
min23asdw <min23asdw23asdw@outlook.com>
2020-12-18 14:23:29 +0700

    /

---
+++ ex3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+int A[10];
+int i = 0;
+while(i < 10){
+ if(i%2==0){A[i]=i;}
+ else{A[i]=i*-1;}
+i++;
+}
+
+i = 0;
+while(i < 10){
+cout << A[i] << " ";
+i++;
+}
+return 0;
+}
